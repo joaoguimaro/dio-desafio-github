963b8cb9cd6f92e73507cc19406dbfb584ca4635
joaoguimaro <jprgui@gmail.com>
2022-05-11 14:03:52 -0300

    Correção e conexão das Demos com a Main no código DemoRef

--- Codigos/DemoRef/Program.cs
+++ Codigos/DemoRef/Program.cs
@@ -1,39 +1,42 @@
-﻿//arrumar e ordenar o código (ta muito confuso)
-using static System.Console;
+﻿using static System.Console;
 public class Program
 {
-    static void Demo6()
+    //Adiciona 20 a uma variavel
+    static void Demo1()
     {
-        int[] numeros = new int[] {0,2,4,6,8};
-        WriteLine($"Digite o número que gostaria de encontrar: ");
-        var numero = int.Parse(ReadLine());
-        var idxEncontrado = EncontrarNumero(numeros, numero);
-
-        if(idxEncontrado >= 0)
-        {
-            WriteLine($"O número digitado está na posição {idxEncontrado}");
-        }
-        else
-        {
-            WriteLine("O número digitado não foi encontrado.");
-        }
+        int a = 2;
+        a = Adicionar20(a);
+        Console.WriteLine($"O valor da variável a é {a}");
     }
-    static void Demo5()
+
+    static int Adicionar20(int x)
     {
-        int[] pares = new int[]{0,2,4,6,8};
-
-        MudarParaImpar(pares);
-
-        WriteLine($"Os impares {string.Join(",", pares)}");
+        return x + 20;
     }
-    static void Demo4()
+
+    //Troca o nome usando classe
+    static void Demo2()
     {
-        string nome = "Ricardo";
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Ricardo";
+        p1.Idade = 30;
+        p1.Documento = "1234";
 
-        TrocarNome2(nome, "José");
+        Pessoa p2 = p1.Clone();
 
-        WriteLine($"O novo nome é {nome}");
+        TrocarNome(p1, "José");
+
+        WriteLine($@"
+        O novo nome é: {p1.Nome}
+        O novo nome é: {p2.Nome}");
     }
+
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+
+    //Troca nome usando struct
     static void Demo3()
     {
         StructPessoa p1 = new StructPessoa()
@@ -51,35 +54,35 @@ public class Program
             Nome do p1 {p1.Nome}
             Nome do p2 {p2.Nome}");
     }
-    static void Demo2()
-    {
-        Pessoa p1 = new Pessoa();
-        p1.Nome = "Ricardo";
-        p1.Idade = 30;
-        p1.Documento = "1234";
-
-        Pessoa p2 = p1.Clone();
-
-        TrocarNome(p1, "José");
-
-        WriteLine($@"
-        O novo nome é: {p1.Nome}
-        O novo nome é: {p2.Nome}");
-    }
-    static void TrocarNome(Pessoa p1, string nomeNovo)
-    {
-        p1.Nome = nomeNovo;
-    }
-
     static StructPessoa TrocarNome1(StructPessoa p1, string nomeNovo1)
     {
         p1.Nome = nomeNovo1;
         return p1;
     }
 
+    //Troca nome usando string
+    static void Demo4()
+    {
+        string nome = "Ricardo";
+
+        TrocarNome2(nome, "José");
+
+        WriteLine($"O novo nome é {nome}");
+    }
+    
     static void TrocarNome2(string nome, string nomeNovo2)
     {
         nome = nomeNovo2;
+    }
+
+    //Pega apenas numeros impares
+    static void Demo5()
+    {
+        int[] pares = new int[]{0,2,4,6,8};
+
+        MudarParaImpar(pares);
+
+        WriteLine($"Os impares {string.Join(",", pares)}");
     }
 
     static void MudarParaImpar(int[] pares)
@@ -87,6 +90,24 @@ public class Program
         for (int i = 0; i < pares.Length; i ++)
         {
             pares[i] = pares[i] + 1;
+        }
+    }
+
+    //Encontra um numero dentro do vetor
+    static void Demo6()
+    {
+        int[] numeros = new int[] {0,2,4,6,8};
+        WriteLine($"Digite o número que gostaria de encontrar: ");
+        var numero = int.Parse(ReadLine());
+        var idxEncontrado = EncontrarNumero(numeros, numero);
+
+        if(idxEncontrado >= 0)
+        {
+            WriteLine($"O número digitado está na posição {idxEncontrado}");
+        }
+        else
+        {
+            WriteLine("O número digitado não foi encontrado.");
         }
     }
 
@@ -100,18 +121,8 @@ public class Program
         return -1;
     }
 
-    static bool EncontarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
-    {
-        foreach(var item in pessoas)
-        {
-            if(item.Nome == pessoa.Nome)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    public static void Main()
+    //Busca um nome em uma lista
+    static void Demo7()
     {
         List<Pessoa> pessoas = new List<Pessoa>()
         {
@@ -133,6 +144,71 @@ public class Program
         else
         {
             WriteLine("Pessoa não localizada!");
+        }
+    }
+    
+    static bool EncontarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
+    {
+        foreach(var item in pessoas)
+        {
+            if(item.Nome == pessoa.Nome)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void Main()
+    {
+        int opcao = 0;
+        WriteLine(@"Digite a opção que deseja executar: 
+                Opção 1 para executar Demo1;
+                Opção 2 para executar Demo2;
+                Opção 3 para executar Demo3;
+                Opção 4 para executar Demo4;
+                Opção 5 para executar Demo5;
+                Opção 6 para executar Demo6;
+                Opção 7 para executar Demo7;
+                Opção 0 para parar de executar");
+
+        opcao = int.Parse(ReadLine());
+
+        while (opcao > 0)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Demo1();
+                    break;
+
+                case 2:
+                    Demo2();
+                    break;
+
+                case 3:
+                    Demo3();
+                    break;
+
+                case 4:
+                    Demo4();
+                    break;
+
+                case 5:
+                    Demo5();
+                    break;
+
+                case 6:
+                    Demo6();
+                    break;
+
+                case 7:
+                    Demo7();
+                    break;
+            }
+
+            WriteLine("\n\n-----------------------//-----------------------");
+            WriteLine("\nDigite a opção que deseja executar: ");
+            opcao = int.Parse(ReadLine());
         }
     }
 }
